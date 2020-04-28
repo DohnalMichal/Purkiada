ece11d266250c8622b5cf29481c78569f145a834
DohnalMichal <michal.dohnal@atlas.cz>
2020-04-28 09:53:07 +0200

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ console.log(`%c
 if ('serviceWorker' in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register('./../../sw.js')
+      .register('../../sw.js')
       .then(reg => console.log("Service Worker: Registered"))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
